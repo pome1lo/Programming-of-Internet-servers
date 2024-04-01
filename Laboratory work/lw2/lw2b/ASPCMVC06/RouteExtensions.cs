c8dd0faeb51853e8127b5be17146b0543d9a4a85
Alexey <95231870+pome1lo@users.noreply.github.com>
2024-04-01 18:07:47 +0300

    Update RouteExtensions.cs

--- Laboratory work/lw2/lw2b/ASPCMVC06/RouteExtensions.cs
+++ Laboratory work/lw2/lw2b/ASPCMVC06/RouteExtensions.cs
@@ -4,86 +4,26 @@ namespace ASPCMVC06
 {
     public static class RouteExtensions
     {
-        public static void ConfigureM01Routes(this WebApplication app)
+        public static void ConfigureRoutes(this WebApplication app)
         {
             app.MapControllerRoute(
-                name: "M01_1",
-                pattern: "/M01",
-                defaults: new { controller = "TMResearch", action = "M01" });
+                name: "V2",
+                pattern: "V2/{controller=TMResearch}/{action=M02}",
+                constraints: new { action = "(?!M03).*" });
 
             app.MapControllerRoute(
-                name: "M01_2",
-                pattern: "TMResearch/M01/{id?}",
-                defaults: new { controller = "TMResearch", action = "M01" });
+                name: "V3",
+                pattern: "V3/{controller=TMResearch}/{string?}/{action=M03}");
 
             app.MapControllerRoute(
-                name: "M01_3",
-                pattern: "/",
-                defaults: new { controller = "TMResearch", action = "M01", id = "" },
-                constraints: new { url = "/" });
-
-            app.MapControllerRoute(
-                name: "M01_4",
-                pattern: "V2/TMResearch/M01",
-                defaults: new { controller = "TMResearch", action = "M01" });
-
-            app.MapControllerRoute(
-                name: "M01_5",
-                pattern: "V3/TMResearch/{id}/M01",
-                defaults: new { controller = "TMResearch", action = "M01", id = "{id}" });
-        }
-
-        public static void ConfigureM02Routes(this WebApplication app)
-        {
-            app.MapControllerRoute(
-                name: "M02_1",
-                pattern: "V2",
-                defaults: new { controller = "TMResearch", action = "M02" });
-
-            app.MapControllerRoute(
-                name: "M02_2",
-                pattern: "V2/TMResearch",
-                defaults: new { controller = "TMResearch", action = "M02" });
-
-            app.MapControllerRoute(
-                name: "M02_3",
-                pattern: "V2/TMResearch/M02",
-                defaults: new { controller = "TMResearch", action = "M02" });
-
-            app.MapControllerRoute(
-                name: "M02_4",
-                pattern: "TMResearch/M02",
-                defaults: new { controller = "TMResearch", action = "M02" });
-
-            app.MapControllerRoute(
-                name: "M02_5",
-                pattern: "V3/TMResearch/{id}/M02",
-                defaults: new { controller = "TMResearch", action = "M02", id = "{id}" });
-        }
-
-        public static void ConfigureM03Routes(this WebApplication app)
-        {
-            app.MapControllerRoute(
-                name: "M03_1",
-                pattern: "V3",
-                defaults: new { controller = "TMResearch", action = "M03" });
-
-            app.MapControllerRoute(
-                name: "M03_2",
-                pattern: "V3/TMResearch/{id}",
-                defaults: new { controller = "TMResearch", action = "M03", id = "{id}" });
-
-            app.MapControllerRoute(
-                name: "M03_3",
-                pattern: "V3/TMResearch/{id}/M03",
-                defaults: new { controller = "TMResearch", action = "M03", id = "{id}" });
-        }
-        public static void ConfigureMXXRoutes(this WebApplication app)
-        {
+                name: "TMResearch",
+                pattern: "{controller=TMResearch}/{action=M01}/{id?}",
+                constraints: new { action = "(?!M03).*" });
+             
             app.MapControllerRoute(
                 name: "MXX",
                 pattern: "{*url}",
                 defaults: new { controller = "TMResearch", action = "MXX" }); 
-        }
+        } 
     }
 }
